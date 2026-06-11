eac3b94483ad34a570ab7b0c52e68970a526b6cf
Eishika Chauhan <chauhaneishika@gmail.com>
2026-06-11 21:11:07 +0530

    Check if array is sorted

---
+++ CheckIfArrayIsSorted.java
@@ -0,0 +1,8 @@
+public class CheckIfArrayIsSorted {
+    public boolean arraySortedOrNot(int[] arr) {
+        for(int i=0; i<arr.length - 1;i++) {
+            if(arr[i] > arr[i+1]) return false;
+        }
+        return true;
+    }
+}
