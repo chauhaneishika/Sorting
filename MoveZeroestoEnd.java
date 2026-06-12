70be22f89398c40219b17c9bb25d72d559016d5c
Eishika Chauhan <chauhaneishika@gmail.com>
2026-06-12 17:54:19 +0530

    Selection Sort

---
+++ MoveZeroestoEnd.java
@@ -0,0 +1,5 @@
+public class MoveZeroestoEnd {
+    public void pushZeroes(int[] nums) {
+        
+    }
+}
---
+++ SelectionSort.java
@@ -0,0 +1,31 @@
+//selects smallest //least no. of swapping
+//time complexity: O(n^2) in all cases
+public class SelectionSort {
+    public static void print(int[] arr) {
+        for(int ele : arr) {
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,-2,6,7,2,0,7,2};
+        int n = arr.length;
+        print(arr);
+        for(int i=0; i<n-1; i++) {
+            int min = Integer.MAX_VALUE;
+            int minIndex = -1;
+            for(int j=i; j<n; j++) {
+                if(arr[j]<min){
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        print(arr);
+    }
+}
