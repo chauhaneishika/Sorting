5faf44d9da487d7b83bbcde0927eae72b0a34fca
eishikachauhan <chauhaneishika@gmail.com>
2026-06-20 16:54:03 +0530

    Kth Smallest

--- KthSmallest.java
+++ KthSmallest.java
@@ -2,8 +2,8 @@ public class KthSmallest {
     public static int kthSmallest(int[] arr, int k) {
         int n = arr.length;
         for(int i=0; i<k; i++) {
-            int min = Integer.MIN_VALUE, minIdx = -1;
-            for(int j=1; j<n; j++){
+            int min = Integer.MAX_VALUE, minIdx = -1;
+            for(int j=i; j<n; j++){
                 if(arr[j] < min){
                     min = arr[j];
                     minIdx = j;
